0b1df6b2a7224c7250ccbb989a2919d29bf9ca63
Tobias Gonzalez <gonzaleztobias09@gmail.com>
2023-09-24 16:00:20 -0300

    ejercicios iniciados

---
+++ EjerciciosIniciado/src/Ejercicio8.java
@@ -0,0 +1,10 @@
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        String nombre = "Tobías";
+        String apel = "González";
+        int edad = 23;
+
+        System.out.printf("Mi nombre es: %s %s %n", apel,nombre);
+        System.out.printf("Mi edad es: %d",edad);
+    }
+}
