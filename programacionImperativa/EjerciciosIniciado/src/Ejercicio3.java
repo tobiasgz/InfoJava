d40bd339a14f1de3496723094edec8b1bdd42754
Tobias Gonzalez <gonzaleztobias09@gmail.com>
2023-09-25 13:57:13 -0300

    probando git

---
+++ programacionImperativa/EjerciciosIniciado/src/Ejercicio3.java
@@ -0,0 +1,15 @@
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        char letra1 = 'T' ;
+        char letra2 = 'o' ;
+        char letra3 = 'b' ;
+        char letra4 = 'i' ;
+        char letra5 = 'a' ;
+        char letra6 = 's' ;
+
+        System.out.printf("Mi nombre es: %c%c%c%c%c%c", letra1,letra2,letra3,letra4,letra5,letra6);
+
+        //La razon se debe a que las comillas dobles java lo interpreta como si fuese una
+        //cadena de caracter, es decir que es algo reservado para la clase String
+    }
+}
---
+++ programacionImperativa/EjerciciosIniciado/src/Ejercicio6.java
@@ -0,0 +1,9 @@
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        String mascota = "Tiger";
+        String pelicula = "Mi Villano Favorito";
+
+        System.out.printf("Nombre de la proxima bebida:\t %s %s", mascota,pelicula);
+
+    }
+}
