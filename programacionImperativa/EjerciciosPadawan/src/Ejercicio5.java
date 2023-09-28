028be161339bf284df7e677cf8390ab36b298cf3
Gonzalez Tobias <gonzaleztobias09@gmail.com>
2023-09-28 09:49:46 -0300

    Delete programacionImperativa directory

--- programacionImperativa/EjerciciosPadawan/src/Ejercicio5.java
+++
@@ -1,8 +0,0 @@
-public class Ejercicio5 {
-    public static void main(String[] args) {
-        final double VALORKG = 0.453592;
-        double libras = 50.0;
-        double conversion = libras * VALORKG;
-        System.out.printf("La conversion de %f Libras a Kilogramos es de: %f kg ", libras, conversion);
-    }
-}
